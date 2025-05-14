aeac367451044e3e5726a44ce2b8a7d07049ceae
kumarlalbaboo <contact.lalbaboo@gmail.com>
2025-05-14 23:12:55 +0530

    added feature

--- src/main/java/com/llb/controllers/ApplicationController.java
+++ src/main/java/com/llb/controllers/ApplicationController.java
@@ -27,7 +27,6 @@ public class ApplicationController {
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Say Hello World");
-        return ResponseEntity.ok("Hello World");
     }
 
 }
